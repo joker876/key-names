c2c79eae0ac8e71bd71181afdf2552d224e58dfc
joker876 <joker876x@gmail.com>
2024-08-23 23:24:55 +0200

    significantly speed up the library

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,21 @@
-import { KEY_MAP } from "./key-maps";
-
+import { KEY_MAP } from './key-maps';
 
 export function keyToString(key: string, useShort: boolean = true) {
-    let lowerKey = key.toLowerCase();
-    
-    for (const MATCHER_ITEM of KEY_MAP) {
-        let matched: boolean = false;
-        //check if matches a certain string
-        if (typeof MATCHER_ITEM.matcher == 'string') {
-            matched = lowerKey == MATCHER_ITEM.matcher;
-        }
-        //check if fulfills a regexp
-        else {
-            matched = MATCHER_ITEM.matcher.test(lowerKey);
-        }
-        if (!matched) continue;
+  const lowerKey = key.toLowerCase();
 
-        if (typeof MATCHER_ITEM.result == 'string') return MATCHER_ITEM.result;
-        if (Array.isArray(MATCHER_ITEM.result)) return useShort ? MATCHER_ITEM.result[0] : MATCHER_ITEM.result[1];
-        return MATCHER_ITEM.result(key, useShort);
-    }
-    //* fallback if none of the predefined matchers were matched
-    //display the side nicely
-    if (lowerKey.match(/^.+(left|right)$/)) {
-        let side = key.match(/right|left/i)![0].toLowerCase();
-        side = side[0].toUpperCase() + side.substring(1);
-        let code = key.replace(/right|left/i, '');
-        key = side + code;
-    }
-    //convert from "PascalCase" to "Title Case"
-    return key.replace(/([A-Z])/, ' $1').trim();
+  if (KEY_MAP[lowerKey]) {
+    const match = KEY_MAP[lowerKey];
+    if (typeof match == 'string') return match;
+    return useShort ? match[0] : match[1];
+  }
+  //! fallback if none of the predefined matchers were matched
+  //display the side nicely
+  if (lowerKey.match(/^.+(left|right)$/)) {
+    let side = key.match(/right|left/i)![0].toLowerCase();
+    side = side[0].toUpperCase() + side.substring(1);
+    let code = key.replace(/right|left/i, '');
+    key = side + code;
+  }
+  //convert from "PascalCase" to "Title Case"
+  return key.replace(/([A-Z])/, ' $1').trim();
 }
--- src/key-maps.ts
+++ src/key-maps.ts
@@ -1,75 +1,95 @@
+type KeyMap = Record<string, string | [string, string]>;
 
+const RAW_KEY_MAP: { keys: string[]; result: string | [string, string] }[] = [
+  { keys: ['tab'], result: 'Tab ↹' },
+  { keys: ['enter'], result: '↵ Enter' },
+  { keys: ['space'], result: ['⎵', 'Space'] },
+  { keys: ['home'], result: 'Home' },
+  { keys: ['end'], result: 'End' },
+  { keys: ['minus'], result: ['-', 'Minus'] },
+  { keys: ['equal'], result: ['=', 'Equal'] },
+  { keys: ['bracketleft'], result: ['[', 'Left Bracket'] },
+  { keys: ['bracketright'], result: [']', 'Right Bracket'] },
+  { keys: ['backslash'], result: ['\\', 'Backslash'] },
+  { keys: ['semicolon'], result: [';', 'Semicolon'] },
+  { keys: ['quote'], result: ["'", 'Quote'] },
+  { keys: ['comma'], result: [',', 'Comma'] },
+  { keys: ['period'], result: ['.', 'Period'] },
+  { keys: ['slash'], result: ['/', 'Slash'] },
+  { keys: ['backquote'], result: ['`', 'Backquote'] },
+  // arrows
+  { keys: ['arrowleft', 'left'], result: ['←', '← Left'] },
+  { keys: ['arrowright', 'right'], result: ['→', '→ Right'] },
+  { keys: ['arrowup', 'up'], result: ['↑', '↑ Up'] },
+  { keys: ['arrowdown', 'down'], result: ['↓', '↓ Down'] },
+  // functional keys
+  { keys: ['capslock', 'caps'], result: 'Caps Lock' },
+  { keys: ['numlock', 'num'], result: 'Num Lock' },
+  { keys: ['shift', 'leftshift', 'shiftleft'], result: 'Shift' },
+  { keys: ['shift', 'rightshift', 'shiftright'], result: 'Right Shift' },
+  { keys: ['ctrl', 'control', 'leftctrl', 'ctrlleft', 'leftcontrol', 'controlleft'], result: ['Ctrl', 'Control'] },
+  {
+    keys: ['ctrl', 'control', 'rightctrl', 'ctrlright', 'rightcontrol', 'controlright'],
+    result: ['Right Ctrl', 'Right Control'],
+  },
+  { keys: ['cmd', 'command'], result: ['Cmd', 'Command'] },
+  { keys: ['option', 'opt'], result: ['Opt', 'Option'] },
+  { keys: ['alt', 'leftalt', 'altleft'], result: 'Alt' },
+  { keys: ['altgraph', 'altgr', 'rightalt', 'altright'], result: ['Alt Gr', 'Alt Graph'] },
+  { keys: ['escape', 'esc'], result: ['Esc', 'Escape'] },
+  { keys: ['insert', 'ins'], result: ['Ins', 'Insert'] },
+  { keys: ['delete', 'del'], result: ['Del', 'Delete'] },
+  { keys: ['windows', 'win', 'leftmeta', 'metaleft'], result: ['⊞ Win', '⊞ Windows'] },
+  { keys: ['menu', 'contextmenu'], result: '▤ Menu' },
+  { keys: ['backspace', 'back', 'bcksp'], result: ['Bcksp', 'Backspace'] },
+  { keys: ['printscreen', 'printsc', 'prtsc'], result: ['Prt Sc', 'Print Screen'] },
+  // numpad
+  { keys: ['numpaddivide', 'divide'], result: ['Numpad /', 'Divide'] },
+  { keys: ['numpadmultiply', 'multiply'], result: ['Numpad *', 'Multiply'] },
+  { keys: ['numpadadd', 'add'], result: ['Numpad +', 'Add'] },
+  { keys: ['numpadsubstract', 'substract'], result: ['Numpad -', 'Substract'] },
+  { keys: ['numpaddecimal', 'decimal'], result: ['Numpad .', 'Decimal'] },
+  { keys: ['numpadenter'], result: '↵ Numpad Enter' },
+  // volume
+  { keys: ['audiovolumemute', 'audiomute', 'volumemute', 'mute'], result: ['Vol. Mute', 'Audio Volume Mute'] },
+  { keys: ['audiovolumedown', 'volumedown'], result: ['Vol. Down', 'Audio Volume Down'] },
+  { keys: ['audiovolumeup', 'volumeup'], result: ['Vol. Up', 'Audio Volume Up'] },
+  // page keys
+  { keys: ['pagedown', 'pgdown'], result: ['Pg Down', 'Page Down'] },
+  { keys: ['pageup', 'pgup'], result: ['Pg Up', 'Page Up'] },
+];
 
-export const KEY_MAP: { matcher: RegExp | string, result: string | [string, string] | ((key: string, useShort: boolean) => string) }[] = [
-    //string matchers
-    { matcher: 'tab', result: 'Tab ↹' },
-    { matcher: 'enter', result: '↵ Enter' },
-    { matcher: 'space', result: 'Space' },
-    { matcher: 'home', result: 'Home' },
-    { matcher: 'end', result: 'End' },
-    { matcher: 'minus', result: ['-', 'Minus'] },
-    { matcher: 'equal', result: ['=', 'Equal'] },
-    { matcher: 'bracketleft', result: ['[', 'Left Bracket'] },
-    { matcher: 'bracketright', result: [']', 'Right Bracket'] },
-    { matcher: 'backslash', result: ['\\', 'Backslash'] },
-    { matcher: 'semicolon', result: [';', 'Semicolon'] },
-    { matcher: 'quote', result: ['\'', 'Quote'] },
-    { matcher: 'comma', result: [',', 'Comma'] },
-    { matcher: 'period', result: ['.', 'Period'] },
-    { matcher: 'slash', result: ['/', 'Slash'] },
-    { matcher: 'backquote', result: ['`', 'Backquote'] },
+const PREMADE_KEY_MAP = RAW_KEY_MAP.reduce<KeyMap>((acc, v) => {
+  for (const key of v.keys) {
+    acc[key] = v.result;
+  }
+  return acc;
+}, {});
 
-    //regex matchers
-    { matcher: /^(arrow)?(left)$/, result: ['←', '← Left' ] },
-    { matcher: /^(arrow)?(right)$/, result: ['→', '→ Right' ] },
-    { matcher: /^(arrow)?(up)$/, result: ['↑', '↑ Up' ] },
-    { matcher: /^(arrow)?(down)$/, result: ['↓', '↓ Down' ] },
-    { matcher: /^caps(lock)?$/, result: 'Caps Lock' },
-    { matcher: /^num(lock)?$/, result: 'Num Lock' },
-    { matcher: /^((left)?shift|shiftleft)$/, result: 'Shift' },
-    { matcher: /^((right)?shift|shiftright)$/, result: 'Right Shift' },
-    { matcher: /^((left)?(ctrl|control)|(ctrl|control)left)$/, result: ['Ctrl', 'Control'] },
-    { matcher: /^((right)?(ctrl|control)|(ctrl|control)right)$/, result: ['Right Ctrl', 'Right Control'] },
-    { matcher: /^(cmd|command)$/, result: ['Cmd', 'Command'] },
-    { matcher: /^opt(ion)?$/, result: ['Opt', 'Option'] },
-    { matcher: /^(altgr(aph)?|altright|rightalt)$/, result: ['Alt Gr', 'Alt Graph'] },
-    { matcher: /^(alt(left)?|leftalt)$/, result: 'Alt' },
-    { matcher: /^esc(ape)?$/, result: ['Esc', 'Escape'] },
-    { matcher: /^ins(ert)?$/, result: ['Ins', 'Insert'] },
-    { matcher: /^del(ete)?$/, result: ['Del', 'Delete'] },
-    { matcher: /^(win(dows)?|metaleft|leftmeta)$/, result: ['⊞ Win', '⊞ Windows'] },
-    { matcher: /^(context)?menu$/, result: '▤ Menu' },
-    { matcher: /^back(space)?$/, result: ['Bcksp', 'Backspace'] },
-    { matcher: /^(printscreen|printsc|prtsc)$/, result: ['Prt Sc', 'Print Screen'] },
-    { matcher: /^(numpad)?divide$/, result: ['Numpad /', 'Divide'] },
-    { matcher: /^(numpad)?multiply$/, result: ['Numpad *', 'Multiply'] },
-    { matcher: /^(numpad)?substract$/, result: ['Numpad -', 'Substract'] },
-    { matcher: /^(numpad)?add$/, result: ['Numpad +', 'Add'] },
-    { matcher: /^(numpad)?decimal$/, result: ['Numpad .', 'Decimal'] },
-    { matcher: /^(numpad)?enter$/, result: '↵ Numpad Enter' },
-    { matcher: /^(audio)?(volume)?mute$/, result: ['Vol. Mute', 'Audio Volume Mute'] },
-    { matcher: /^(audio)?volumedown$/, result: ['Vol. Down', 'Audio Volume Down'] },
-    { matcher: /^(audio)?volumeup$/, result: ['Vol. Up', 'Audio Volume Up'] },
+// create keys for all the letters
+const LETTER_KEY_MAP = 'abcdefghijklmnopqrstuvwxyz'.split('').reduce<KeyMap>((acc, v) => {
+  acc[`key${v}`] = v.toUpperCase();
+  return acc;
+}, {});
 
-    //function result
-    { 
-        matcher: /^key[a-z]$/,
-        result: key => key.substring(key.length - 1).toUpperCase()
-    },
-    { 
-        matcher: /^digit\d$/,
-        result: key => key.substring(key.length - 1)
-    },
-    { 
-        matcher: /^f\d\d?$/,
-        result: key => key.toUpperCase()
-    },
-    { 
-        matcher: /^numpad\d$/,
-        result: key => `Numpad ${key.substring(key.length - 1)}`
-    },
-    { 
-        matcher: /^(page|pg)(up|down)$/, 
-        result: (key, useShort) => useShort ? key.replace(/page|pg/i, 'Pg ') : key.replace(/page|pg/i, 'Page ')
-    },
-]
+// create keys for all the numbers (and numpad numbers)
+const DIGIT_KEY_MAP: KeyMap = {};
+for (let i = 0; i < 10; i++) {
+  const str = i.toString();
+  DIGIT_KEY_MAP[`digit${str}`] = str;
+  DIGIT_KEY_MAP[`numpad${str}`] = `Numpad ${str}`;
+}
+
+// create keys for all function keys (F1, F2, ..., F12)
+const F_KEY_MAP: KeyMap = {};
+for (let i = 1; i <= 12; i++) {
+  const str = i.toString();
+  F_KEY_MAP[`f${str}`] = str;
+}
+
+export const KEY_MAP: KeyMap = {
+  ...PREMADE_KEY_MAP,
+  ...LETTER_KEY_MAP,
+  ...DIGIT_KEY_MAP,
+  ...F_KEY_MAP,
+};
